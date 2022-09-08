8ee1f166f1a74c30885907609eadf2a779e2d81e
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 11:09:16 +0300

    Update AddSimpleTaskTest.java

--- src/test/java/tests/AddSimpleTaskTest.java
+++ src/test/java/tests/AddSimpleTaskTest.java
@@ -8,7 +8,7 @@ import pageObjects.TasksPage;
 public class AddSimpleTaskTest extends BaseTest{
 	
 	@Severity(SeverityLevel.NORMAL)
- 	@Test (description="Add new simple task")
+ 	@Test(description="Add new simple task")
 	public void tc01_addNewSimpleTask() {
 		TasksPage tp = new TasksPage(driver);
 		tp.addTask("First task added");
@@ -18,7 +18,7 @@ public class AddSimpleTaskTest extends BaseTest{
 	}
 	
 	@Severity(SeverityLevel.NORMAL)
- 	@Test (dataProvider="getData", description="Add 3 new simple task")
+ 	@Test(dataProvider="getData", description="Add 3 new simple task")
 	public void tc02_addNewSimpleTasks(String taskName) {
 		TasksPage tp = new TasksPage(driver);
 		tp.addTask(taskName);
