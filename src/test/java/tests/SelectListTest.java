24bcdac4974b7db4efb1182f81d2d1a73b6ab845
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 11:03:21 +0300

    Update SelectListTest.java

--- src/test/java/tests/SelectListTest.java
+++ src/test/java/tests/SelectListTest.java
@@ -7,6 +7,7 @@ import pageObjects.TasksPage;
 
 public class SelectListTest extends BaseTest{
 
+	@Severity(SeverityLevel.NORMAL)
 	@Test (dataProvider="getData", description="Add a new list")
 	public void tc01_addNewList(String list) {
 		TasksPage tp = new TasksPage(driver);
@@ -15,15 +16,16 @@ public class SelectListTest extends BaseTest{
 		Assert.assertTrue(tp.isListExist(list));
 	}
 
-	 @Test (dataProvider="getData", description="Select list")
-	 public void tc02_selectList(String list) {
+	@Severity(SeverityLevel.NORMAL)
+	@Test (dataProvider="getData", description="Select list")
+	public void tc02_selectList(String list) {
 		TasksPage tp = new TasksPage(driver);
 		tp.selectList(list);
 		//Validation
 		String actual = tp.getListText();
 		String expected = list;
 		Assert.assertEquals(actual, expected);		 	
-	 }
+	}
 
 	@DataProvider
 	public Object[][] getData(){
