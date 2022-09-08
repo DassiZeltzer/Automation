cd4bdddf1eaa203e9d0728f91057bcfaeddce2ff
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 11:04:58 +0300

    Update FilterTest.java

--- src/test/java/tests/FilterTest.java
+++ src/test/java/tests/FilterTest.java
@@ -7,14 +7,15 @@ import pageObjects.TasksPage;
 
 public class FilterTest extends BaseTest{
 	
-	 @Test (description="Filter tasks by tag")
-	 public void tc01_filterByTag() {
-	 	TasksPage tp = new TasksPage(driver);
-	 	tp.openAdvancedTask();
-	 	AdvancedTaskPage atp = new AdvancedTaskPage(driver);
+	@Severity(SeverityLevel.NORMAL)
+	@Test (description="Filter tasks by tag")
+	public void tc01_filterByTag() {
+		TasksPage tp = new TasksPage(driver);
+		tp.openAdvancedTask();
+		AdvancedTaskPage atp = new AdvancedTaskPage(driver);
 	 	atp.addAdvancedTask("0", "3/3/2014", "advanced task", "note", "dassi");
 	 	tp.filterByTag("dassi");
-	 	//Validation
-	 	Assert.assertTrue(tp.isFilterResultsContainTag("dassi"));
-	 }
+		//Validation
+		Assert.assertTrue(tp.isFilterResultsContainTag("dassi"));
+	}
 }
