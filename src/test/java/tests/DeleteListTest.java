263f44c6cb0b7d6a00e8f5ae2195b206f343c68c
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 10:02:41 +0300

    Update DeleteListTest.java

--- src/test/java/tests/DeleteListTest.java
+++ src/test/java/tests/DeleteListTest.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 import pageObjects.TasksPage;
 
 public class DeleteListTest extends BaseTest{
-	   @Test
-	 	public void tc01_deleteNewList() {
-	 		TasksPage tp = new TasksPage(driver);
-	 		tp.addList("Example list");
-	 		tp.clickOnList("Example list");
-	 		tp.deleteList("Example list");
-	 		//Validation
-	 		Assert.assertFalse(tp.isListExist("Example list"));
-	 	}
+	 @Test
+	 public void tc01_deleteNewList() {
+	 	TasksPage tp = new TasksPage(driver);
+	 	tp.addList("Example list");
+	 	tp.clickOnList("Example list");
+	 	tp.deleteList("Example list");
+	 	//Validation
+	 	Assert.assertFalse(tp.isListExist("Example list"));
+ 	}
 }
