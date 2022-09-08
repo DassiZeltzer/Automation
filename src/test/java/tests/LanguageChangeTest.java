a53bfcf74505740e178d15c87caf825741ee5bd5
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 10:32:18 +0300

    Update LanguageChangeTest.java

--- src/test/java/tests/LanguageChangeTest.java
+++ src/test/java/tests/LanguageChangeTest.java
@@ -6,33 +6,30 @@ import pageObjects.SettingsPage;
 import pageObjects.TasksPage;
 
 public class LanguageChangeTest extends BaseTest{
-	 @Test
-	 public void tc01_hebrewLanguageChange() {
+	
+	@Test (description="Change the website language to French")
+	public void tc01_FrenchLanguageChange() {
 	 	TasksPage tp = new TasksPage(driver);
-	 	tp.goToSettings();
-
-	 	SettingsPage sp = new SettingsPage(driver);
-	 	sp.chooseLanguage("fr");
-
+		tp.goToSettings();
+		SettingsPage sp = new SettingsPage(driver);
+		sp.chooseLanguage("fr");
 	 	//Validation option 1
-	 	Assert.assertTrue(tp.isLanguageChanged("Configuration"));
-	 	//Validation option 2
+		Assert.assertTrue(tp.isLanguageChanged("Configuration"));
+		//Validation option 2
 		String actual = tp.getSettingsText();
 		String expected = "Configuration";
 		Assert.assertEquals(actual, expected);
 	 }
 	   
-	@Test
+	@Test (description="Change the website language to danish")
 	public void tc02_danishLanguageChange() {
-		 TasksPage tp = new TasksPage(driver);
-		 tp.goToSettings();
-		 	
-		 SettingsPage sp = new SettingsPage(driver);
-		 sp.chooseLanguage("da");
-		 	
-		 //Validation option 1
-		 Assert.assertTrue(tp.isLanguageChanged("Indstillinger"));
-		 //Validation option 2
+		TasksPage tp = new TasksPage(driver);
+		tp.goToSettings();
+		SettingsPage sp = new SettingsPage(driver);
+		sp.chooseLanguage("da");	
+		//Validation option 1
+		Assert.assertTrue(tp.isLanguageChanged("Indstillinger"));
+		//Validation option 2
 		String actual = tp.getSettingsText();
 		String expected = "Indstillinger";
 		Assert.assertEquals(actual, expected);
