6edffba1f8d44326a0de46ecf7fc308c29b70cfa
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 10:03:46 +0300

    Update DeleteTaskTest.java

--- src/test/java/tests/DeleteTaskTest.java
+++ src/test/java/tests/DeleteTaskTest.java
@@ -2,17 +2,15 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import pageObjects.TasksPage;
 
 public class DeleteTaskTest extends BaseTest{
-	   @Test
-	 	public void tc01_deleteTask() {
-	 		TasksPage tp = new TasksPage(driver);
-	 		tp.addTask("Null 1");
-	 		tp.deleteTask("Null 1");
-	 		//Validation
-	 		Assert.assertFalse(tp.isTaskExist("Null 1"));
-	 	}
-
+	@Test
+	public void tc01_deleteTask() {
+		TasksPage tp = new TasksPage(driver);
+		tp.addTask("Null 1");
+		tp.deleteTask("Null 1");
+		//Validation
+	 	Assert.assertFalse(tp.isTaskExist("Null 1"));
+	}
 }
