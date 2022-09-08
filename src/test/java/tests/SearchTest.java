38f1e6b4c3a7bf8305fdc27bf35ce672d0a9a077
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 11:03:39 +0300

    Update SearchTest.java

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -6,6 +6,7 @@ import pageObjects.TasksPage;
 
 public class SearchTest extends BaseTest{
 
+	@Severity(SeverityLevel.NORMAL)
 	@Test (description="Search exising task")
 	public void tc01_searchExisingTask() {	
 		TasksPage tp = new TasksPage(driver);
@@ -14,6 +15,7 @@ public class SearchTest extends BaseTest{
 		Assert.assertTrue(tp.isSearchResultsContain("First task"));
 	}
 	
+	@Severity(SeverityLevel.NORMAL)
 	@Test (description="Search missing task")
 	public void tc02_searchMissingTask() {
 		TasksPage tp = new TasksPage(driver);
