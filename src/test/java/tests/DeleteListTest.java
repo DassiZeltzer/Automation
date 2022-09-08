bfc33ab7bd5860803dadcad5c472a501f7392c44
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 11:05:48 +0300

    Update DeleteListTest.java

--- src/test/java/tests/DeleteListTest.java
+++ src/test/java/tests/DeleteListTest.java
@@ -6,13 +6,14 @@ import pageObjects.TasksPage;
 
 public class DeleteListTest extends BaseTest{
 	
-	 @Test (description="Delete an existing list")
-	 public void tc01_deleteNewList() {
-	 	TasksPage tp = new TasksPage(driver);
-	 	tp.addList("Example list");
-	 	tp.clickOnList("Example list");
-	 	tp.deleteList("Example list");
-	 	//Validation
-	 	Assert.assertFalse(tp.isListExist("Example list"));
+	@Severity(SeverityLevel.NORMAL)
+	@Test (description="Delete an existing list")
+	public void tc01_deleteNewList() {
+		TasksPage tp = new TasksPage(driver);
+		tp.addList("Example list");
+		tp.clickOnList("Example list");
+		tp.deleteList("Example list");
+		//Validation
+		Assert.assertFalse(tp.isListExist("Example list"));
  	}
 }
