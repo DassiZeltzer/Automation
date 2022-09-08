66b8bfb31ce8b2e75e03fb0e1389894a97354c6f
DassiZeltzer <105452516+DassiZeltzer@users.noreply.github.com>
2022-09-08 09:44:57 +0300

    Update AdvancedTaskPage.java

--- src/test/java/pageObjects/AdvancedTaskPage.java
+++ src/test/java/pageObjects/AdvancedTaskPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class AdvancedTaskPage extends BasePage{
 	@FindBy(css="[name='prio']")
 	private WebElement priorityList;
@@ -33,7 +32,5 @@ public class AdvancedTaskPage extends BasePage{
 		fillText(tagsField, Tags);
 		click(saveButton);
 		sleep(1000);
-
 	}
-
 }
